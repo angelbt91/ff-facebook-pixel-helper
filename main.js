c8401fba8a07788970f71e49143560c6e8eb4e6b
Angel Blanco <angelbt91@gmail.com>
2020-09-16 09:33:54 +0200

    Add basic FB event capturing

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-let fbq2 = fbq;
+window.onload = function () {
+    document.body.style.border = "5px solid red"; // helper to debug if ext is running
 
-let eventCatcher = function(value) {
-    console.log("Llamada capturada:", value);
-};
+    let originalFbq = window.wrappedJSObject.fbq;
 
-fbq = function(task, event, params = null) {
-    eventCatcher(`${task}, ${event}, ${params}`);
+    newFbq = function (task, event, params = undefined) {
+        eventCatcher(`${task}, ${event}, ${params}`);
 
-    if (params) {
-        fbq2(task, event, params);
-    } else {
-        fbq2(task, event);
-    }
-};
+        if (params) {
+            originalFbq(task, event, params);
+        } else {
+            originalFbq(task, event);
+        }
+    };
+
+    // replace former fbq object in the page by our newFbq
+    window.wrappedJSObject.fbq = cloneInto(
+        newFbq,
+        window,
+        {cloneFunctions: true});
+
+    let eventCatcher = function (value) {
+        console.log("Llamada capturada:", value);
+    };
+}
