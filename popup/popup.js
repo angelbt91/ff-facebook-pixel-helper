c44c1d1dca17c810e210c9463a833599c0e909d9
Angel Blanco <angelbt91@gmail.com>
2020-09-17 20:51:24 +0200

    Add basic event report painting in the popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,26 @@
-let events;
-
 browser.tabs.query({active: true, currentWindow: true}, function (tabs) {
     browser.tabs.sendMessage(tabs[0].id, {type: "getEvents"}, function (eventsReceived) {
-        events = eventsReceived;
-
-        // TODO paint the events in the popup
+        paintReport(eventsReceived);
     });
 });
+
+const paintReport = function (events) {
+    const rootNode = document.getElementById("root");
+
+    events.forEach(function (event) {
+        const block = document.createElement("div");
+        const action = event.param0.toLowerCase();
+        let text = document.createElement("p");
+
+        if (action === "init") {
+            text.textContent = "PIXEL INITIATED";
+        } else if (action === "track") {
+            text.textContent = `EVENT FIRED: ${event.param1}.`;
+        } else if (action === "trackcustom") {
+            text.textContent = `CUSTOM EVENT FIRED: ${event.param1}.`
+        }
+
+        block.appendChild(text);
+        rootNode.appendChild(block);
+    });
+}
