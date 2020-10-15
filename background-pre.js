3d1bb235f9e35cc5bf26646c53447544fa92d8d3
Angel Blanco <angelbt91@gmail.com>
2020-10-15 21:40:57 +0200

    Adapt background script file architecture

--- background-pre.js
+++ background-pre.js
@@ -13,7 +13,7 @@ function registerCalls(details) {
         fbRequests.push({
             "tabId": details.tabId,
             "documentUrl": details.documentUrl,
-            "events": [details.url]
+            "events": [formatEvent(details.url)]
         })
     } else {
         // if we changed the page inside the same tab, we remove the calls stored for that tab and start again
@@ -21,8 +21,15 @@ function registerCalls(details) {
             fbRequests[index].documentUrl = details.documentUrl;
             fbRequests[index].events = [];
         }
-        fbRequests[index].events.push(details.url);
+        fbRequests[index].events.push(formatEvent(details.url));
     }
+
+    console.log(fbRequests); // debugging purposes, remove
+}
+
+function formatEvent(url) {
+    const queryString = require('query-string');
+    return queryString.parseUrl(url);
 }
 
 browser.webRequest.onCompleted.addListener(registerCalls, {urls: ["*://*.facebook.com/*", "*://*.facebook.net/*"]});
